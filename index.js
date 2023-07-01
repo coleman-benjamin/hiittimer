2f0d12bb368b001a826637f37c297f56953012ca
Benjamin Coleman <colemanjben@gmail.com>
2023-07-01 11:59:13 -0300

    clean up, refactor, add coin audio

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+/**
+ * CONSTANTS
+ */
+const BTN_START = 'start';
+const BTN_PAUSE = 'pause';
+const BTN_RESET = 'reset';
+const INPUT_INTERVAL = 'interval';
+const INPUT_REST = 'rest';
+const INPUT_REPETITIONS = 'repetitions';
+const DIV_TIMER = 'timer';
+const AUDIO_COIN = 'coin.mp3';
+
+
+/**
+ * STATE
+ */
 const state = {
     startTime: null,
     timer: null,
@@ -11,6 +27,66 @@ const state = {
     currentRepetitions: null,
 }
 
+
+/**
+ * HELPERS
+ */
+function getElement(id) {
+    return document.getElementById(id);
+}
+
+function getValue(id) {
+    return getElement(id).value;
+}
+
+
+/**
+ * BUTTON STATES
+ */
+function setStartBtnPressedState() {
+    getElement(BTN_START).disabled = true;
+    getElement(BTN_PAUSE).disabled = false;
+    getElement(BTN_RESET).disabled = false;
+}
+
+function setResetBtnPressedState() {
+    getElement(BTN_START).disabled = false;
+    getElement(BTN_PAUSE).disabled = true;
+    getElement(BTN_RESET).disabled = true;
+}
+
+
+/**
+ * TIMER
+ */
+function calculateTimer() {
+    const elapsedTime = (Date.now() - state.startTime) / 1000;
+    state.currentInterval = (state.interval - elapsedTime).toFixed(3);
+}
+
+function startTimer() {
+    state.timer = setInterval(function () {
+        calculateTimer();
+        render();
+    }, 100);
+}
+
+function stopTimer() {
+    clearInterval(state.timer);
+}
+
+
+/**
+ * BUTTON EVENT HANDLERS
+ */
+function setFromInput() {
+    state.interval = getValue(INPUT_INTERVAL);
+    state.rest = getValue(INPUT_REST);
+    state.repetitions = getValue(INPUT_REPETITIONS);
+    state.currentRepetitions = getValue(INPUT_REPETITIONS);
+    state.currentInterval = getValue(INPUT_INTERVAL);
+}
+
 function start() {
     if (state.timer && !state.paused) {
         return;
@@ -21,43 +97,34 @@ function start() {
         state.startTime = Date.now();
     }
 
-    state.timer = setInterval(function () {
-        calculateTimer();
-        render();
-    }, 100);
-}
-
-function setFromInput() {
-    state.interval = getInput('interval');
-    state.repetitions = getInput('repetitions');
-    state.rest = getInput('rest');
-
-    state.currentRepetitions = getInput('repetitions');
-    state.currentInterval = getInput('interval');
-}
-
-function calculateTimer() {
-    const elapsedTime = (Date.now() - state.startTime) / 1000;
-    state.currentInterval = (state.interval - elapsedTime).toFixed(3);
+    setStartBtnPressedState();
+    startTimer();
 }
 
 function pause() {
     state.paused = true;
-    clearInterval(state.timer);
+    stopTimer();
+    beep();
 }
 
 function reset() {
     state.timer = null;
     state.paused = false;
-    clearInterval(state.timer);
+    stopTimer();
     setFromInput();
     render();
+    setResetBtnPressedState();
 }
 
-function getInput(id) {
-    return document.getElementById(id).value;
+
+/**
+ * A/V
+ */
+function beep() {
+    const audio = new Audio(AUDIO_COIN);
+    audio.play();
 }
 
 function render() {
-    document.getElementById('timer').innerHTML = state.currentInterval;
+    getElement(DIV_TIMER).innerHTML = state.currentInterval;
 }
